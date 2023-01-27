d73ef83c4d6f01fad3729a32b1c82ee682203d85
bravindan <53007654+bravindan@users.noreply.github.com>
2023-01-27 20:03:41 +0300

    Update App.js
    Added contact section

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import {
   AiFillLinkedin,
   AiFillYoutube,
   AiFillInstagram,
+  AiFillGithub
 } from "react-icons/ai";
 import displaypic from "./images/brav.jpeg";
 import code from "./images/code.png";
 import exp from "./images/exp.png";
 import design from "./images/design.png";
+import phone from "./images/phone.svg"
+import whatsapp from "./images/whatsapp.svg"
+import email from "./images/email.svg"
 import { useState } from "react";
+import {FaWhatsapp} from 'react-icons/fa'
 import './index.css'
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
-  if(darkMode){
-    localStorage.setItem("!darkMode","dark");
-  }else{
-    localStorage.getItem("!darkMode");
-  }
+  // if(darkMode){
+  //   localStorage.setItem("!darkMode","dark");
+  // }else{
+  //   localStorage.getItem("!darkMode");
   
   return (
     <div className={darkMode ? "dark" : ""}>
@@ -62,25 +66,27 @@ function App() {
             </p>
           </div>
           <div className="flex justify-center gap-12 py-2 text-3xl text-gray-700 dark:text-teal-600 cursor-pointer">
-            <AiFillTwitterCircle
-              onClick={(event) =>
-                (window.location.href = "https://twitter.com/Bravin_Dan")
-              }
-            />
-            <AiFillInstagram onClick={(event) => (window.location.href = "https://www.instagram.com/brav_dan/")} />
-            <AiFillLinkedin
-              onClick={(event) =>
-                (window.location.href =
-                  "https://www.linkedin.com/in/bravin-daniel-bw")
-              }
-            />
-            <AiFillYoutube
-              onClick={(event) =>
-                (window.location.href =
-                  "https://youtube.com/channel/UC6AZdR8ngHeT7oy6T8_g4qQ")
-              }
-            />
-          </div>
+         <AiFillTwitterCircle
+             onClick={(event) =>
+               (window.location.href = "https://twitter.com/Bravin_Dan")
+             }
+           />
+           <AiFillInstagram onClick={(event) => (window.location.href = "https://www.instagram.com/brav_dan/")} />
+           <AiFillLinkedin
+             onClick={(event) =>
+               (window.location.href =
+                 "https://www.linkedin.com/in/bravin-daniel-bw")
+             }
+           />
+           <AiFillYoutube
+             onClick={(event) =>
+               (window.location.href =
+                 "https://youtube.com/channel/UC6AZdR8ngHeT7oy6T8_g4qQ")
+             }
+           />
+           <AiFillGithub onClick={(event)=>(window.location.href="https://github.com/bravindan")}/>
+         </div>
+        
           <div className="relative mx-auto mt-20 mx w-80 h-80 md:h-96 md:w-96">
             {/* <img src={process.env.PUBLIC_URL+"brav.jpeg"} alt='mypic'/> */}
             <img className="rounded-full" src={displaypic} alt="mypic" />
@@ -180,6 +186,56 @@ function App() {
               </p>
             </div>
           </div>
+
+          {/* contact info section */}
+          <h3 className="py-1 text-3xl text-center mx-auto dark:text-gray-400">
+              Contact Me
+            </h3>
+          <div className="gap-8 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3">
+            <div className="p-10 my-10 text-center shadow-lg rounded-xl dark:text-gray-400 dark:shadow-slate-700 dark:shadow-md hover:-translate-y-1 hover:scale-110 duration-300 ">
+              <img
+                className="mx-auto"
+                src={email}
+                alt=""
+                width={100}
+                height={100}
+              />
+              <h3 className="py-1 pt-8 pb-2 text-lg font-medium ">Email</h3>
+              
+             <a href="mailto:danielbravin0@gmail.com"><h4 className="py-4 text-teal-600 bg-slate-300 rounded-md">Message</h4></a>
+             
+            </div>
+            <div className="p-10 my-10 text-center shadow-lg rounded-xl dark:text-gray-400 dark:shadow-slate-700 dark:shadow-md hover:-translate-y-1 hover:scale-110 duration-300 ">
+              <img
+                className="mx-auto"
+                src={whatsapp}
+                alt={<FaWhatsapp/>}
+                width={100}
+                height={100}
+              />
+              
+              <h3 className="py-1 pt-8 pb-2 text-lg font-medium">WhatsApp</h3>
+             
+              <a href="https://wa.me/+254701634107"><h4 className="py-4 text-teal-600 bg-slate-300 rounded-md">Message</h4></a>
+              
+            </div>
+            <div className="p-10 my-10 text-center shadow-lg rounded-xl dark:text-gray-400 dark:shadow-slate-700 dark:shadow-md hover:-translate-y-1 hover:scale-110 duration-300 ">
+              <img
+                className="mx-auto"
+                src={phone}
+                alt={<FaWhatsapp/>}
+                width={100}
+                height={100}
+              />
+              {/* MODIFY: */}
+              <h3 className="py-1 pt-8 pb-2 text-lg font-medium">Phone</h3>
+              <a href="tel:0701634107"><h4 className="py-4 text-teal-600 bg-slate-300 rounded-md">Call</h4></a>
+              
+             
+        
+            </div>
+          </div>
+          {/* end of contact info section */}
         </section>
         <section>
           <footer className="text-center py-5 pt-8 dark:text-gray-400 text-sm">
